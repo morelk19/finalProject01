3758b454ab9262b20a81e8f4ff4021c0b6a968bd
morelk19 <morelkhristian@gmail.com>
2022-12-13 07:12:25 -0500

    got it somewhat working

--- bundle.js
+++ bundle.js
@@ -1,89 +1,231 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 
-
-
+const gameArea = document.querySelector("#gameArea");
 var ch = require('chess.js');
 
 
-const chess = new ch.Chess()
+const game = new ch.Chess()
 
 
+createBoard();
 
+const pieces = document.querySelectorAll("#gameArea img");
+let dragged = null;
 
-var board = null
-var game = new ch.Chess()
+for(let piece of pieces){
+  piece.addEventListener("dragstart", (event) => {
+    // store a ref. on the dragged elem
 
+    console.log(game.turn());
+    console.log(event.target.classList[2]);
+    
+    if (game.isGameOver()) return false
 
-function onDragStart (source, piece, position, orientation) {
-  // do not pick up pieces if the game is over
-  if (game.isGameOver()) return false
+    // only pick up pieces for the side to move
+    if ((game.turn() === 'w' && event.target.classList[2] === 'white') ||
+        (game.turn() === 'b' && event.target.classList[2] === 'black')) {
+          dragged = event.target;
+    }else
+      return false;
 
-  // only pick up pieces for the side to move
-  if ((game.turn() === 'w' && piece.search(/^b/) !== -1) ||
-      (game.turn() === 'b' && piece.search(/^w/) !== -1)) {
-    return false
-  }
+    
+  });
 }
 
-function onDrop (source, target) {
-  // see if the move is legal
-  var move = game.move({
-    from: source,
-    to: target,
-    promotion: 'q' // NOTE: always promote to a queen for example simplicity
-  })
-
-  // illegal move
-  if (move === null) return 'snapback'
-
-  updateStatus()
+const board = document.querySelectorAll("#gameArea .dropzone");
+for(let square of board){
+  square.addEventListener("dragover", (event) => {
+    // prevent default to allow drop
+    event.preventDefault();
+  });
 }
 
-// update the board position after the piece snap
-// for castling, en passant, pawn promotion
-function onSnapEnd () {
-  board.position(game.fen())
-}
 
-function updateStatus () {
-  var status = ''
 
-  var moveColor = 'White'
-  if (game.turn() === 'b') {
-    moveColor = 'Black'
-  }
+for(let square of board){
+    square.addEventListener("drop", (event) => {
+// prevent default action (open as link for some elements)
+    event.preventDefault();
+// move dragged element to the selected drop target
+    if (event.target.classList[1] === "dropzone") {
+      console.log(dragged.id)
+      console.log(event.target.classList[3])
+      var move = game.move({
+        from: dragged.id.toString(),
+        to: event.target.classList[3].toString(),
+      })
+      console.log(game.board());
 
-  // checkmate?
-  if (game.inCheckmate()) {
-    status = 'Game over, ' + moveColor + ' is in checkmate.'
-  }
-
-  // draw?
-  else if (game.isDraw()) {
-    status = 'Game over, drawn position'
-  }
-
-  // game still on
-  else {
-    status = moveColor + ' to move'
-
-    // check?
-    if (game.inCheck()) {
-      status += ', ' + moveColor + ' is in check'
+      if (move === null) return 'snapback'
+    dragged.parentNode.removeChild(dragged);
+    event.target.appendChild(dragged);
     }
+  });
+}
+
+
+function createBoard(){
+    var table = document.createElement("div");
+    table.classList.add(`container`);
+for (var i = 1; i < 9; i++) {
+    var r = document.createElement("div");
+    r.classList.add(`row`);
+    for (var j = 1; j < 9; j++) {
+        var c = document.createElement("div");
+        c.classList.add(`col`);
+        //c.classlist[0]
+        c.classList.add('dropzone');
+        //c.classlist[1]
+        if (i%2 == j%2) {
+            c.classList.add("white");
+            //c.classlist[2]
+        } else {
+            c.classList.add("black");
+            //c.classlist[2]
+        }
+        let column = '';
+        let row  = '';
+        if(i === 1){
+            row = 8;
+        }else if(i === 2){
+            row = 7;
+        }else if(i === 3){
+            row = 6;
+        }else if(i === 4){
+          row = 5;
+        }else if(i === 5){
+            row = 4;
+        }else if(i === 6){
+            row = 3;
+        }else if(i === 7){
+            row = 2;
+        }else if(i === 8){
+            row = 1;
+        }
+        if(j === 1){
+            column = 'a';
+        }else if(j === 2){
+            column = 'b';
+        }else if(j === 3){
+            column = 'c';
+        }else if(j === 4){
+            column = 'd';
+        }else if(j === 5){
+            column = 'e';
+        }else if(j === 6){
+            column = 'f';
+        }else if(j === 7){
+            column = 'g';
+        }else if(j === 8){
+            column = 'h';
+        }
+       c.classList.add(`${column}${row}`);
+       //c.classlist[3]
+       c.classList.add(`${i},${j}`);
+       //c.classlist[4]
+  
+        r.appendChild(c);
+        setBoad(`${i},${j}`, c, `${column}${row}`);   
+       
+    }
+    table.appendChild(r);
+}
+gameArea.appendChild(table);
+}
+function getCoord(classCoords){
+    let coords = classCoords.split(",");
+    return coords;
+}
+function setImage(src, width, height, alt, imgQuerySelector, coords, color, chessCoords) {
+    var img = document.createElement("img");
+    img.src = src;
+    img.width = width;
+    img.height = height;
+    img.setAttribute(`id`, `${chessCoords}`);
+    img.alt = alt;
+    img.setAttribute('draggable', true);
+    img.classList.add("dragdemo");
+    //img.classList[0]
+    img.classList.add("draggable");
+    //img.classList[1]
+    img.classList.add(`${color}`);
+    //img.classList[2]
+    img.classList.add(`${coords}`);
+     //img.classList[3]
+    // This next line will just add it to the <body> tag
+    imgQuerySelector.appendChild(img);
+}   
+function setBoad(classCoords, querSel, chessCoords){
+    let coords = getCoord(classCoords);
+    if(coords[0] === "1" && coords[1] ==="1"){
+        setImage("images/blackRook.jpeg", 20, 20, "black rook", querSel, classCoords, `black`, chessCoords);
+    }else if(coords[0] === "1" && coords[1] ==="2"){
+        setImage("images/blackKnight.jpeg", 20, 20, "black knight", querSel, classCoords, `black`, chessCoords);
+    }else if(coords[0] === "1" && coords[1] ==="3"){
+        setImage("images/blackBishop.jpeg", 20, 20, "black bishop", querSel, classCoords, `black`, chessCoords);
+    }else if(coords[0] === "1" && coords[1] ==="4"){
+        setImage("images/blackQueen.jpeg", 20, 20, "black queen", querSel, classCoords, `black`, chessCoords);
+    }else if(coords[0] === "1" && coords[1] ==="5"){
+        setImage("images/blackKing.jpeg", 20, 20, "black king", querSel, classCoords, `black`, chessCoords);
+    }else if(coords[0] === "1" && coords[1] ==="6"){
+        setImage("images/blackBishop.jpeg", 20, 20, "black bishop", querSel, classCoords, `black`, chessCoords);
+    }else if(coords[0] === "1" && coords[1] ==="7"){
+    setImage("images/blackKnight.jpeg", 20, 20, "black knight", querSel, classCoords, `black`, chessCoords);
+    }else if(coords[0] === "1" && coords[1] ==="8"){
+        setImage("images/blackRook.jpeg", 20, 20, "black rook", querSel, classCoords, `black`, chessCoords);
+    }else if(coords[0] === "2" && coords[1] ==="1"){
+        setImage("images/blackPawn.jpeg", 20, 20, "black pawn", querSel, classCoords, `black`, chessCoords);
+    }else if(coords[0] === "2" && coords[1] ==="2"){
+        setImage("images/blackPawn.jpeg", 20, 20, "black pawn", querSel, classCoords, `black`, chessCoords);
+    }else if(coords[0] === "2" && coords[1] ==="3"){
+        setImage("images/blackPawn.jpeg", 20, 20, "black pawn", querSel, classCoords, `black`, chessCoords);
+    }else if(coords[0] === "2" && coords[1] ==="4"){
+        setImage("images/blackPawn.jpeg", 20, 20, "black pawn", querSel, classCoords, `black`, chessCoords);
+    }else if(coords[0] === "2" && coords[1] ==="5"){
+        setImage("images/blackPawn.jpeg", 20, 20, "black pawn", querSel, classCoords, `black`, chessCoords);
+    }else if(coords[0] === "2" && coords[1] ==="6"){
+        setImage("images/blackPawn.jpeg", 20, 20, "black pawn", querSel, classCoords, `black`, chessCoords);
+    }else if(coords[0] === "2" && coords[1] ==="7"){
+    setImage("images/blackPawn.jpeg", 20, 20, "black pawn", querSel, classCoords, `black`, chessCoords);
+    }else if(coords[0] === "2" && coords[1] ==="8"){
+        setImage("images/blackPawn.jpeg", 20, 20, "black pawn", querSel, classCoords, `black`, chessCoords);
+    }else if(coords[0] === "8" && coords[1] ==="1"){
+        setImage("images/whiteRook.jpeg", 20, 20, "white rook", querSel, classCoords, `white`, chessCoords);
+    }else if(coords[0] === "8" && coords[1] ==="2"){
+        setImage("images/whiteKnight.jpeg", 20, 20, "white knight", querSel, classCoords, `white`, chessCoords);
+    }else if(coords[0] === "8" && coords[1] ==="3"){
+        setImage("images/whiteBishop.jpeg", 20, 20, "white bishop", querSel, classCoords, `white`, chessCoords);
+    }else if(coords[0] === "8" && coords[1] ==="4"){
+        setImage("images/whiteQueen.jpeg", 20, 20, "white queen", querSel, classCoords, `white`, chessCoords);
+    }else if(coords[0] === "8" && coords[1] ==="5"){
+        setImage("images/whiteKing.jpeg", 20, 20, "white king", querSel, classCoords, `white`, chessCoords);
+    }else if(coords[0] === "8" && coords[1] ==="6"){
+        setImage("images/whiteBishop.jpeg", 20, 20, "white bishop", querSel, classCoords, `white`, chessCoords);
+    }else if(coords[0] === "8" && coords[1] ==="7"){
+    setImage("images/whiteKnight.jpeg", 20, 20, "white knight", querSel, classCoords, `white`, chessCoords);
+    }else if(coords[0] === "8" && coords[1] ==="8"){
+        setImage("images/whiteRook.jpeg", 20, 20, "white rook", querSel, classCoords, `white`, chessCoords);
+    }else if(coords[0] === "7" && coords[1] ==="1"){
+        setImage("images/whitePawn.jpeg", 20, 20, "white pawn", querSel, classCoords, `white`, chessCoords);
+    }else if(coords[0] === "7" && coords[1] ==="2"){
+        setImage("images/whitePawn.jpeg", 20, 20, "white pawn", querSel, classCoords, `white`, chessCoords);
+    }else if(coords[0] === "7" && coords[1] ==="3"){
+        setImage("images/whitePawn.jpeg", 20, 20, "white pawn", querSel, classCoords, `white`, chessCoords);
+    }else if(coords[0] === "7" && coords[1] ==="4"){
+        setImage("images/whitePawn.jpeg", 20, 20, "white pawn", querSel, classCoords, `white`, chessCoords);
+    }else if(coords[0] === "7" && coords[1] ==="5"){
+        setImage("images/whitePawn.jpeg", 20, 20, "white pawn", querSel, classCoords, `white`, chessCoords);
+    }else if(coords[0] === "7" && coords[1] ==="6"){
+        setImage("images/whitePawn.jpeg", 20, 20, "white pawn", querSel, classCoords, `white`, chessCoords);
+    }else if(coords[0] === "7" && coords[1] ==="7"){
+    setImage("images/whitePawn.jpeg", 20, 20, "white pawn", querSel, classCoords, `white`, chessCoords);
+    }else if(coords[0] === "7" && coords[1] ==="8"){
+      setImage("images/whitePawn.jpeg", 20, 20, "white pawn", querSel, classCoords, `white`, chessCoords);
+    }
+  };
+  function move(piece, location){
+      
   }
-}
-
-var config = {
-  draggable: true,
-  position: 'start',
-  onDragStart: onDragStart,
-  onDrop: onDrop,
-  onSnapEnd: onSnapEnd
-}
-board = Chessboard('myBoard', config)
-
-updateStatus()
 },{"chess.js":2}],2:[function(require,module,exports){
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
